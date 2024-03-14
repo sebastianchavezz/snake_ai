37fa65d2e1bf96b8701c35e7f7d962b0830479f7
sebastianchavezz <42916343+sebastianchavezz@users.noreply.github.com>
2024-03-14 01:48:53 +0100

    yep

--- weights/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../includes/snake.h"
-#include "../includes/agent.h"
-#include "../includes/gym.h
-#include "../includes/game_logic.h"
-#include "../includes/game_render.h"
-
-int main(){
-    
-}
